7471f1f86884d297de7e1c4e02e0f7618ddb7264
000SergeyMayer000 <samajor@mail.ru>
2020-02-13 14:34:00 +0300

    4.4. Максимум из трех чисел[#235421]

---
+++ chapter_001/src/test/java/ru/job4j_2/condition/MultiMaxTest.java
@@ -0,0 +1,21 @@
+package ru.job4j_2.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, -3, -10);
+        assertThat(result, is(1));
+    }
+}
