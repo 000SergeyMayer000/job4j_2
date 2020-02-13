e65d9fd28181d2fa00b8f40c8cc10715e8adc649
000SergeyMayer000 <samajor@mail.ru>
2020-02-13 13:29:52 +0300

    правил Read.my

---
+++ chapter_001/src/main/java/ru/job4j_2/condition/AlertDivByZero.java
@@ -0,0 +1,20 @@
+package ru.job4j_2.condition;
+
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(0);
+        possibleDiv(-5);
+    }
+
+    public static void possibleDiv(int number) {
+        if (number == 0) {
+            System.out.println(number + " Could not div by 0.");
+        }
+        if (number < 0) {
+            System.out.println(number + " This is negative numbers");
+        }
+    }
+}
+
+
