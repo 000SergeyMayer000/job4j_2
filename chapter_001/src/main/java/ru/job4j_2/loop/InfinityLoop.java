fc78aff2e6dba0e63d6c7ae67d7f4dc6771b071c
000SergeyMayer000 <samajor@mail.ru>
2020-02-14 00:04:00 +0300

    5.0.2. Вечный цикл for.[#235501]

---
+++ chapter_001/src/main/java/ru/job4j_2/loop/InfinityLoop.java
@@ -0,0 +1,11 @@
+package ru.job4j_2.loop;
+
+    public class InfinityLoop {
+        public static void main(String[] args) {
+            for (int index = 0; index < 10; index++) {
+                System.out.println("infinity");
+                index = index - 1; //лишняя строчка
+            }
+        }
+    }
+
