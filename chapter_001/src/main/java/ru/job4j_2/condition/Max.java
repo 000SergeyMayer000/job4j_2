d2d611314c372f13edbc42df1a4c7d8cb9ec63b8
000SergeyMayer000 <samajor@mail.ru>
2020-02-13 13:56:57 +0300

    4.2. Максимум из двух чисел[#235420]

---
+++ chapter_001/src/main/java/ru/job4j_2/condition/Max.java
@@ -0,0 +1,8 @@
+package ru.job4j_2.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left;
+        return result = left > right? left : right;
+    }
+}
