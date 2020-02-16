f17942fc720f7fe8d64b8d507d939d8015772937
000SergeyMayer000 <samajor@mail.ru>
2020-02-16 23:33:53 +0300

    6.0.2. Заполнение массива.[#235440]

--- chapter_001/src/main/java/ru/job4j_2/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j_2/array/ArrayDefinition.java
@@ -13,5 +13,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Peter Parker";
+        names[1] = "Peter Arsentiev";
+        names[2] = "Peter Griffin";
+        names[3] = "Peter Pan";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
