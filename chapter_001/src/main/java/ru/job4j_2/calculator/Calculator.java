4b57da1e321ec63e3f0e044ef259f9fd47eede22
000SergeyMayer000 <samajor@mail.ru>
2020-02-16 22:14:39 +0300

    5.8. JavaDoc. Code convention[#235505]

--- chapter_001/src/main/java/ru/job4j_2/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j_2/calculator/Calculator.java
@@ -1,6 +1,15 @@
 package ru.job4j_2.calculator;
 
+/**
+ * класс Calculator  - пример выполнения арифметических операций
+ * @ author - Mayer Sergej
+ *
+ */
 public class Calculator {
+    /**
+     *  Main
+     * @param args - String[] args
+     */
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
---
+++ chapter_001/src/main/java/ru/job4j_2/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Sergey Mayer (samajor@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j_2.calculator;
