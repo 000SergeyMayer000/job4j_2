f5a9eae1ae618387245e0115aeff45733c8754b5
000SergeyMayer000 <samajor@mail.ru>
2020-02-17 23:11:09 +0300

    6.5.3. Сортировка выборкой.[#235434]

--- chapter_001/src/main/java/ru/job4j_2/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j_2/array/FindLoop.java
@@ -42,4 +42,22 @@ public class FindLoop {
         }
         return rst;
     }
+
+    /**
+     * 6.5.3. Сортировка выборкой.[#235434]
+     * @param - сортируемый массив
+     * @return - отсортированнный массив
+     */
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexOf(data, min, 0, data.length - 1);
+            int tmp = data[i];
+            data[i] = data[index];
+            data[index] = tmp;
+        }
+        return data;
+    }
+
+
 }
--- chapter_001/src/test/java/ru/job4j_2/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j_2/array/FindLoopTest.java
@@ -40,7 +40,7 @@ public class FindLoopTest {
 
     @Test
     public void whenFind3() {
-        int[] input = new int[] {5, 2, 10, 2, 4};
+        int[] input = new int[]{5, 2, 10, 2, 4};
         int value = 2;
         int start = 2;
         int finish = 4;
@@ -51,10 +51,10 @@ public class FindLoopTest {
 
     @Test
     public void whenFind0() {
-        int[] input = new int[] {0, 5, 2, 0, 10, 2, 4, 0};
+        int[] input = new int[]{0, 5, 2, 0, 10, 2, 4, 0};
         int value = 0;
         int start = 4;
-        int finish =7;
+        int finish = 7;
         int result = FindLoop.indexOf(input, value, start, finish);
         int expect = 7;
         assertThat(result, is(expect));
@@ -62,14 +62,28 @@ public class FindLoopTest {
 
     @Test
     public void whenFind2() {
-        int[] input = new int[] {0, 5, 2, 0, 10, 2, 4, 0};
+        int[] input = new int[]{0, 5, 2, 0, 10, 2, 4, 0};
         int value = 2;
         int start = 2;
-        int finish =5;
+        int finish = 5;
         int result = FindLoop.indexOf(input, value, start, finish);
         int expect = 2;
         assertThat(result, is(expect));
     }
 
+    @Test
+    public void whenSort34125() {
+        int[] input = new int[]{3, 4, 1, 2, 5};
+        int[] result = FindLoop.sort(input);
+        int[] expect = new int[]{1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
 
+    @Test
+    public void whenSortNegative123456() {
+        int[] input = new int[]{-1, -2, -3, -4, -5, -6};
+        int[] result = FindLoop.sort(input);
+        int[] expect = new int[]{-6, -5, -4, -3, -2, -1};
+        assertThat(result, is(expect));
+    }
 }
