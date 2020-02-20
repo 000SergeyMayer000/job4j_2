89816d57f1102eaffd5d9c43669cd7bce1fe6936
000SergeyMayer000 <samajor@mail.ru>
2020-02-20 10:16:51 +0300

    4.5. Отладка программы в IDEA[#235418]        replace

--- chapter_001/src/main/java/ru/job4j2/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j2/condition/SqMax.java
@@ -7,8 +7,6 @@ public class SqMax {
             if (first > third) {
                 if (first > forth) {
                     result = first;
-                } else {
-                    return result;
                 }
             } else if (third > forth) {
                 result = third;
