43f2b9127cabe6297ea9cb90b816583089671468
000SergeyMayer000 <samajor@mail.ru>
2020-02-25 17:33:44 +0300

    1.1. Создание объекта[#235594]

---
+++ chapter_002/src/main/java/ru/job4j2/oop/Dog.java
@@ -0,0 +1,17 @@
+package ru.job4j2.oop;
+
+/**
+ * 1.1. Создание объекта[#235594]
+ */
+public class Dog {
+
+    /**
+     * в методе main демонстрируетс ясоздание объектов класса Dog
+     * @param args - args
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhuchka = new Dog();
+    }
+}
