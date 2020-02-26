e756c9380f8ccfcf1b513085ac02533c144ac129
000SergeyMayer000 <samajor@mail.ru>
2020-02-26 11:17:53 +0300

    2.1. Рефакторинг - Расстояние между точками.[#235590]

--- chapter_001/src/main/java/ru/job4j2/converter/Point.java
+++ chapter_001/src/main/java/ru/job4j2/converter/Point.java
@@ -1,30 +1,55 @@
 package ru.job4j2.converter;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 /**
  * 7. Расстояние между точками в системе координат[#235463]
  */
 public class Point {
 
     /**
-     * метод находит расстояне мужду двух точек с заданными координатами
-     *
-     * @param x1 - абсцисса первой точки
-     * @param y1 - ордината первой точки
-     * @param x2 - абсциса второй точки
-     * @param y2 - ордината второй точки
-     * @return - расстояние
+     * Это поле объекта. Оно доступно только конкретному объекту.
      */
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
-        return rsl;
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+
+    /**
+     * конструктор - особый метод класса, инициализирующий состояние объекта во время его создания
+     *
+     * @param first  - первый параметр - X
+     * @param second - второй параметр - Y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
 
     /**
+     * метод определяет расстояние от точки на которой
+     * вызван метод до точки, которая передается в качестве параметра
+     *
+     * @param that - вторая точка
+     * @return - растояние
+     */
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+
+    /**
      * Main
+     *
      * @param args - args
      */
     public static void main(String[] args) {
-        double result = Point.distance(0, 3, 4, 0);
-        System.out.println("result (0, 3) to (4, 0) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
