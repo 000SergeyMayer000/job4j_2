2251d87911017ea62c8b57c82a34fca562c82403
000SergeyMayer000 <samajor@mail.ru>
2020-02-26 15:28:17 +0300

    2. Перегрузка конструктора.[#235606]

--- chapter_002/src/main/java/ru/job4j2/tracker/Animal.java
+++ chapter_002/src/main/java/ru/job4j2/tracker/Animal.java
@@ -1,8 +1,14 @@
 package ru.job4j2.tracker;
 
 public class Animal {
+    String name;
     public Animal() {
         super();
+        System.out.println("Animal");
+    }
+
+    public Animal(String name) {
+        this.name = name;
         System.out.println("Animal");
     }
 }
