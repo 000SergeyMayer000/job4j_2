671b20fbf38a1d7f636433d097e25261afa6b34e
000SergeyMayer000 <samajor@mail.ru>
2020-03-11 00:27:01 +0300

    9.1 Рефакторинг теста @Before @After[#235560]

--- chapter_002/src/test/java/ru/job4j2/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j2/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j2.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -12,16 +14,26 @@ import java.util.StringJoiner;
 
 
 public class PaintTest {
+
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().painting(new Square());
-        // проверяем результат вычисления
         assertThat(
                 new String(out.toByteArray()),
                 is(
@@ -33,7 +45,21 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().painting(new Triangle());
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringJoiner(System.lineSeparator())
+                                .add("   +   ")
+                                .add("  + +  ")
+                                .add(" +   + ")
+                                .add("+++++++")
+                                .toString()
+                )
+        );
     }
 }
