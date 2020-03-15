43fa4e201ec1fca955ad94c91f5c7ba9bcfde91e
000SergeyMayer000 <samajor@mail.ru>
2020-03-15 22:08:35 +0300

    6.8. Дефрагментация массива.[#235447] ++

--- chapter_001/src/main/java/ru/job4j2/array/Defragment.java
+++ chapter_001/src/main/java/ru/job4j2/array/Defragment.java
@@ -11,22 +11,57 @@ public class Defragment {
      * @param array - исходный массив
      * @return - конечный массив
      */
+
     public static String[] compress(String[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == null) {
-                int point = i;
-                while (point < array.length - 1) {
-                    if (array[point + 1] != null) {
-                        array[i] = array[point + 1];
-                        array[point + 1] = null;
-                        break;
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+                while (array[index] == null) {
+                    if (array[point] != null) {
+                        String count = array[point];
+                        array[point] = array[index];
+                        array[index] = count;
                     }
                     point++;
+                    if (point == array.length) {
+                        break;
+                    }
                 }
             }
         }
         return array;
     }
+
+//    public static String[] compress(String[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] == null) {
+//                int point = i;
+//                while (point < array.length - 1) {
+//                    if (array[point + 1] != null) {
+//                        array[i] = array[point + 1];
+//                        array[point + 1] = null;
+//                        break;
+//                    }
+//                    point++;
+//                }
+//            }
+//        }
+//        return array;
+//    }
+
+//      public static String[] compress(String[] array) {
+//            int pointer = -1; // указатель на последнюю ячейку со значением
+//            for (int c = 0; c < array.length; c++) {
+//                if (array[c] != null) {
+//                    array[++pointer] = array[c];
+//                }
+//            }
+//            if (pointer > -1) {
+//                for (int c = pointer + 1; c < array.length; c++)
+//                    array[c] = null;
+//            }
+//            return array;
+//        }
 
     /**
      * Main
