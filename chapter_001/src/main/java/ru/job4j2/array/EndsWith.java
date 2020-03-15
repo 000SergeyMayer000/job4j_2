c27dd81c603c52b8936b09f6818cc9b81d94a38f
000SergeyMayer000 <samajor@mail.ru>
2020-03-15 21:58:40 +0300

    6.4.1. Слово заканчивается с ...[#235432] +

--- chapter_001/src/main/java/ru/job4j2/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j2/array/EndsWith.java
@@ -12,6 +12,16 @@ public class EndsWith {
      * @return -
      */
 
+//    public static boolean endsWith(char[] word, char[] post) {
+//        boolean result = true;
+//        for (int i = (post.length - 1); i >= 0; i--) {
+//            if (post[i] != (word[(word.length - post.length) + i])) {
+//                result = false;
+//                break;
+//            }
+//        }
+//        return result;
+//    }
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int index = 0; index < post.length; index++) {
