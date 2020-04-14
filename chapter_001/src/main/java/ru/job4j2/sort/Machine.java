50b044961248e208d9ea62b84a379ce74536bd19
000SergeyMayer000 <samajor@mail.ru>
2020-04-14 20:09:58 +0300

    мелкие правки Checkstyle

--- chapter_001/src/main/java/ru/job4j2/sort/Machine.java
+++ chapter_001/src/main/java/ru/job4j2/sort/Machine.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * Сдача в кофе машине. [#34741]
  */
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     /**
      * метод реализует работу лендингов аппарата, расчитывает сдачу в монетах
@@ -19,10 +19,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int change = money - price;
-        for (int i = 0; i < COINS.length; i++) {
-            while (change >= COINS[i]) {
-                change -= COINS[i];
-                rsl[size++] = COINS[i];
+        for (int i = 0; i < coins.length; i++) {
+            while (change >= coins[i]) {
+                change -= coins[i];
+                rsl[size++] = coins[i];
             }
         }
         return Arrays.copyOf(rsl, size);
